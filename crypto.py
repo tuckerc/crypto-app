21a276f9d011bc73a61861ae042d48f5f6b90687
Chase Tucker <tucchase@amazon.com>
2020-08-18 22:35:28 -0700

    encrypt decrypt both working

--- crypto.py
+++ crypto.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 
-from os import environ
+from os import environ, remove, mkdir
 from os.path import splitext
 from pathlib import Path
+from shutil import rmtree
 import aws_encryption_sdk
 import boto3
 from flask import Flask, render_template, request, send_file
@@ -30,24 +31,47 @@ def encrypt_file(file):
     return ct_filename
 
 
-@app.route("/")
-@app.route("/upload")
-def upload():
-    return render_template("upload.html")
+# method for decrypting uploaded file
+def decrypt_file(file, filetype):
+    pt_filename = str(splitext(file)[0]) + "." + filetype
+    with open(file, 'rb') as ct_file, open(pt_filename, 'wb') as pt_file:
+        with aws_encryption_sdk.stream(
+                mode='d',
+                source=ct_file,
+                key_provider=kms_key_provider
+        ) as decryptor:
+            for chunk in decryptor:
+                pt_file.write(chunk)
+    return pt_filename
 
 
-@app.route("/uploader", methods=["GET", "POST"])
-def upload_file():
+@app.route("/", methods=["GET"])
+@app.route("/upload", methods=["POST"])
+def load():
+    rmtree("files")
+    mkdir("files")
     if request.method == "GET":  # if method is a get (same as "/upload")
-        return render_template("upload.html")
-    if request.method == "POST":
+        return render_template("load.html")
+    elif request.method == "POST":
         f = request.files["file"]
         path = Path("files")
         file = path / f.filename
         f.save(file)
-        ct_file = encrypt_file(file)
-        return send_file(ct_file)
+        if splitext(file)[1] == '.ct':
+            filetype = request.values["filetype"]
+            pt_file = decrypt_file(file, filetype)
+            remove(file)
+            return send_file(pt_file, as_attachment=True)
+        else:
+            ct_file = encrypt_file(file)
+            remove(file)
+            return send_file(ct_file, as_attachment=True)
         # return render_template("download.html", file=ct_file)
+
+
+# @app.route("/uploader", methods=["GET", "POST"])
+# def upload_file():
+
 
 
 if __name__ == "__main__":
